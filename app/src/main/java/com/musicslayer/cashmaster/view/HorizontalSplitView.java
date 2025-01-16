3ef8433ea46312db39af420933462d7ffff6ade1
musicslayer <musicslayer@gmail.com>
2025-01-16 16:16:17 -0500

    Updates to LayoutParams

--- app/src/main/java/com/musicslayer/cashmaster/view/HorizontalSplitView.java
+++ app/src/main/java/com/musicslayer/cashmaster/view/HorizontalSplitView.java
@@ -3,6 +3,7 @@ package com.musicslayer.cashmaster.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 // This view is made from two LinearLayouts such that they each take up half of the available horizontal width.
@@ -29,7 +30,6 @@ public class HorizontalSplitView extends LinearLayout {
 
     public void makeLayout() {
         this.setOrientation(HORIZONTAL);
-        this.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         this.setWeightSum(1.0f);
 
         Context context = getContext();
@@ -44,5 +44,15 @@ public class HorizontalSplitView extends LinearLayout {
 
         this.addView(LA);
         this.addView(LB);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        ViewGroup.LayoutParams params = this.getLayoutParams();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        this.setLayoutParams(params);
     }
 }
