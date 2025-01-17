33d3be185d026768ec0d762548dd55069bce7ab2
musicslayer <musicslayer@gmail.com>
2025-01-17 00:08:06 -0500

    Update

--- app/src/main/java/com/musicslayer/cashmaster/activity/InitialActivity.java
+++ app/src/main/java/com/musicslayer/cashmaster/activity/InitialActivity.java
@@ -9,7 +9,7 @@ import com.musicslayer.cashmaster.data.persistent.app.YearLedgerList;
 import com.musicslayer.cashmaster.ledger.YearLedger;
 import com.musicslayer.cashmaster.util.ToastUtil;
 
-// TODO Just manually use tables
+// TODO Color Themes!
 
 // This Activity class only exists for initialization code, not to be seen by the user.
 public class InitialActivity extends BaseActivity {
