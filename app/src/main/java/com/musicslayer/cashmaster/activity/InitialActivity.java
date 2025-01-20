a68fceaf0c3c7e1b1000967795a818c8d385e2f9
musicslayer <musicslayer@gmail.com>
2025-01-20 14:48:13 -0500

    Update commons-io version

--- app/src/main/java/com/musicslayer/cashmaster/activity/InitialActivity.java
+++ app/src/main/java/com/musicslayer/cashmaster/activity/InitialActivity.java
@@ -10,6 +10,8 @@ import com.musicslayer.cashmaster.data.persistent.app.YearLedgerList;
 import com.musicslayer.cashmaster.ledger.YearLedger;
 import com.musicslayer.cashmaster.util.ToastUtil;
 
+// TODO Merge Theme/Appearance/Color
+
 // This Activity class only exists for initialization code, not to be seen by the user.
 public class InitialActivity extends BaseActivity {
     @Override
